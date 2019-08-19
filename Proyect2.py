df73c5a5a5b6d306bec243611ec73b65bfbdcbc3
IsaiDJAJ <iza_cr46@hotmail.com>
2019-08-19 18:33:37 -0500

    Aquí subo los cambios que hice al código

--- Proyect2.py
+++ Proyect2.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QColorDialog, QFontDialog, QFileDialog, QLabel, QSizePolicy, QScrollArea
-from PyQt5.QtGui import QIcon, QPixmap, QFontDatabase, QFontMetrics
+from PyQt5.QtGui import QIcon, QPixmap, QFontDatabase, QFontMetrics, QFont
 from PyQt5.QtCore import pyqtSlot, QRect
 from PyQt5.QtGui import QColor
 from PIL import Image, ImageDraw, ImageFont
@@ -13,7 +13,7 @@ class App(Ui_MainWindow, QtWidgets.QMainWindow):
         super().__init__()
         self.setupUi(self)
 
-        self.label.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+#        self.label.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
         self.label.setScaledContents(True)
         self.scrollArea.setBackgroundRole(QPalette.Dark)
         self.scrollArea.setVisible(True)
@@ -21,6 +21,7 @@ class App(Ui_MainWindow, QtWidgets.QMainWindow):
         self.myFont = self.fontComboBox.currentFont()
         self.Font = pygame.font.match_font(self.myFont.family())
         self.spinBox.setValue(11)  # Tamaño de letra predeterminado en el textEdit.
+        self.textEdit.setFontPointSize(self.spinBox.value()*(0.7808))
 
         self.actionNew_2.triggered.connect(self.insertImage)
         self.pushButtonColor.clicked.connect(self.openColorDialog)
@@ -37,9 +38,11 @@ class App(Ui_MainWindow, QtWidgets.QMainWindow):
     def changeFont(self):
         self.textEdit.selectAll()
         self.myFont = QtGui.QFont(self.fontComboBox.itemText(self.fontComboBox.currentIndex()))
+        self.myFont.setPointSize(self.spinBox.value()*(0.7808))
         self.textEdit.setFont(self.myFont)
         self.textEdit.setFocus()
-        self.l_TD.setText(self.textEdit.toHtml())
+#        self.l_TD.setText(self.textEdit.toHtml())
+        self.l_TD.setFont(self.myFont)
         self.l_TD.adjustSize()
         self.Font = pygame.font.match_font(self.myFont.family())
 ##        print(pygame.font.SysFont(self.myFont.family(), 11))
@@ -50,7 +53,7 @@ class App(Ui_MainWindow, QtWidgets.QMainWindow):
     def size(self):
         self.textEdit.selectAll()
         self.mySize=self.spinBox.value()
-        self.textEdit.setFontPointSize(self.mySize)
+        self.textEdit.setFontPointSize(self.mySize*(0.7808))
 
     def insertImage(self):
         options = QFileDialog.Options()
@@ -65,25 +68,25 @@ class App(Ui_MainWindow, QtWidgets.QMainWindow):
             self.label.move(-1, -1)
             self.scrollAreaWidgetContents.resize(self.im.width(), self.im.height())
 #            self.scrollArea.setVisible(True)
+            self.l_TD.setMaximumWidth(self.scrollAreaWidgetContents.width() - 10)
+            self.l_TD.setMaximumHeight(self.scrollAreaWidgetContents.height() - 10)
             self.xp = self.im.width() / 2
             self.yp = self.im.height() / 2
-            print(str(self.xp), str(self.yp))
+#            print(str(self.xp), str(self.yp))
             self.l_TD.move(self.xp - (self.l_TD.width() * 0.5), self.yp - (self.l_TD.height() * 0.5))
-            print(str(self.l_TD.width()/2), str(self.l_TD.height()/2))
-            print(str(self.l_TD.x()), str(self.l_TD.y()))
-            print(str(self.l_TD.pos()))
+#            print(str(self.l_TD.width()/2), str(self.l_TD.height()/2))
+#            print(str(self.l_TD.x()), str(self.l_TD.y()))
+#            print(str(self.l_TD.pos()))
 
     def DisplayInLabel(self):
-        self.l_TD.setMaximumWidth(self.scrollAreaWidgetContents.width()-10)
-        self.l_TD.setMaximumHeight(self.scrollAreaWidgetContents.height()-10)
-        self.l_TD.setText(self.textEdit.toHtml())
+        self.l_TD.setText(self.textEdit.toPlainText())
+        self.myFont.setPointSize(self.spinBox.value() * (0.7808))
+        self.l_TD.setFont(self.myFont)
+#        self.l_TD.setText(self.textEdit.toHtml())
 #        print(self.textEdit.toHtml())
-##        self.l_TD.setWordWrap(True)
+        self.l_TD.setWordWrap(True)
         self.l_TD.adjustSize()
-
-        if self.label.x() < 0 and self.label.y() < 0:
-            self.l_TD.move(self.xp - (self.l_TD.width() * 0.5), self.yp - (self.l_TD.height() * 0.5))
-
+        self.l_TD.move((self.scrollAreaWidgetContents.width()*0.5) - (self.l_TD.width() * 0.5), (self.scrollAreaWidgetContents.height()*0.5) - (self.l_TD.height() * 0.5))
         self.textEdit.setFocus()
 
 #        if self.l_TD.width() > (self.label.width()-10):
@@ -94,13 +97,17 @@ class App(Ui_MainWindow, QtWidgets.QMainWindow):
     def WriteOnImage(self):
         image = Image.open(self.files)
         draw = ImageDraw.Draw(image)
-        font = ImageFont.truetype(self.Font, size=int(self.spinBox.value()+5))
-        (x, y) = (self.l_TD.x()+3, self.l_TD.y())
         text = self.textEdit.toPlainText()
+        font = ImageFont.truetype(self.Font, size=int(self.spinBox.value()))
+        w , h = draw.textsize(text, font)
+        (x, y) = (int((self.im.width()*0.5)-(w*0.5)-0.5), int((self.im.height()*0.5)-(h*0.5)-2))
 #        text = self.label.text()
-        print(self.label.text())
+#        print(text)
+#        print(str(self.textEdit.toPlainText()))
         rgb = (self.color.red(), self.color.green(), self.color.blue())
         color = "rgb(%d,%d,%d)" % rgb
+        print("Dimensiones del Texto "+ str(w) + "," + str(h))
+        print("Dimensiones del Label "+ str(self.l_TD.width()) + "," + str(self.l_TD.height()))
         draw.text((x, y), text, fill=color, font=font)
 
         image.save('Imagen1.png')
@@ -128,7 +135,7 @@ class Preview(QWidget):
         pixmap = QPixmap(self.Image)
         label.setPixmap(pixmap)
         self.resize(pixmap.width(), pixmap.height())
-        print(str(pixmap.width()), str(pixmap.height()))
+#        print(str(pixmap.width()), str(pixmap.height()))
         self.show()
 
 if __name__ == "__main__":
